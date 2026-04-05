e66ebda9e0de345133ccf05b221968734ebf2473
yuu <kaites5030bb@gmail.com>
2026-04-05 23:41:05 +0900

    削除

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function greet(){
   console.log("Hello from the local branch");
   console.log("Hello from the local branch");
-  console.log("Hello from the local branch");
-  console.log("Hello from the local branch");
 }
